6ca539a29c75cbc0bec282f71d905d3790ec45b9
fufupatwo <aaronadelgadoo@gmail.com>
2024-03-06 17:28:04 -0600

    Calendar of Date Ideas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>7 days of dates</h1>
+      <h2>Welcome to 7 Days of Dates, Annalisa! Check out this calendar to pick and choose date ideas!</h2>
+      
+    </div>
+  )
+}
+
+export default App
