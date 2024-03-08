b455fa927ea5527ed1c11b7dd535f1597b3ddf89
fufupatwo <aaronadelgadoo@gmail.com>
2024-03-08 15:59:30 -0600

    testing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Calendar from './components/Calendar';
 
 const App = () => {
 
@@ -6,7 +7,7 @@ const App = () => {
     <div className="App">
       <h1>7 days of dates</h1>
       <h2>Welcome to 7 Days of Dates, Annalisa! Check out this calendar to pick and choose date ideas!</h2>
-      
+      <Calendar />
     </div>
   )
 }
